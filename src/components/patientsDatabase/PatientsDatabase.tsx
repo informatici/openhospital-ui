37661e784d3cf6d549c03f4c911a424ea22fe61e
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-08-09 16:53:40 +0200

    refactor: adjust indentation

--- src/components/patientsDatabase/PatientsDatabase.tsx
+++ src/components/patientsDatabase/PatientsDatabase.tsx
@@ -29,224 +29,188 @@ import AddIcon from '@material-ui/icons/Add';
 import CancelIcon from '@material-ui/icons/Cancel';
 import Breadcrumbs from '@material-ui/lab/Breadcrumbs';
 
-export interface Props extends WithStyles<typeof styles> { }
+export interface Props extends WithStyles<typeof styles> {}
 
 interface State {
-  error: any;
-  isLoaded: boolean;
-  items: any[];
-  selectedDate: any;
-  patients: Array<Patient>;
-  visible: Number;
-  searchedValue: String;
-  isDeleteDialogOpen: boolean;
+    error: any;
+    isLoaded: boolean;
+    items: any[];
+    selectedDate: any;
+    patients: Array<Patient>;
+    visible: Number;
+    searchedValue: String;
+    isDeleteDialogOpen: boolean;
  
 }
 
 class PatientsDatabase extends Component<Props, State> {
-  state: State = {
-    error: null,
-    isLoaded: false,
-    items: [],
-    selectedDate: new Date(),
-    isDeleteDialogOpen: false,
-  };
+    state: State = {
+        error: null,
+        isLoaded: false,
+        items: [],
+        selectedDate: new Date(),
+        isDeleteDialogOpen: false,
+    };
    
-  componentDidMount() {
-    const patientController: PatientControllerApi = new PatientControllerApi();
-    const requestParams: GetPatientsUsingGETRequest = {
-      page: 1,
-      size: 8,
+    componentDidMount() {
+        const patientController: PatientControllerApi = new PatientControllerApi();
+        const requestParams: GetPatientsUsingGETRequest = { page: 1, size: 8 }
+
+        patientController.getPatientsUsingGET(requestParams).then(
+            (result) => {
+                this.setState({ isLoaded: true, items: result, });
+            },
+            (error) => {
+              this.setState({ isLoaded: true, error });
+            }
+        )
+    }
+  
+    handleClickOpen = () => {
+        this.setState({ isDeleteDialogOpen: true });
+    };
+  
+    handleClickClose = () => {
+        this.setState({ isDeleteDialogOpen: false });
+    };
+
+    keywordInput = (classes, classNames) => {
+        return (
+            <Grid item xs={12} sm={3}>
+                <TextField
+                    id="keyword"
+                    label="Keyword"
+                    className={classNames(classes.formField, classes.cssOutlinedInput)}
+                    placeholder="First name, last name, tax number..."
+                    InputLabelProps={{
+                        classes: {
+                            root: classes.formFieldInputLabel,
+                            focused: classes.cssFocused,
+                        },
+                    }}
+                    InputProps={{
+                        classes: {
+                            root: classes.formFieldInput,
+                            notchedOutline: classes.cssOutlinedInput,
+                        },
+                    }}
+                    margin="normal"
+                    variant="outlined"/>
+            </Grid>
+        )
     }
 
-    patientController.getPatientsUsingGET(requestParams)
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result,
+    public render() {
+        const { classes, theme } = this.props;
+        const { items, isLoaded, error, isDeleteDialogOpen } = this.state;
+        const patients = (
+            items && items.length !== 0 ?
+                (items.map((item) => (<Patients info={item}/>)))
+            :
+                <CircularProgress className={classes.progress} color="secondary" style={{ margin: '20px auto' }} />
+        )
 
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+        return (
 
-    this.setState({
-      // labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
-    });
-  }
-  
-  
-
-
-  handleClickOpen = () => {
-    this.setState({ isDeleteDialogOpen: true });
-  };
-  
-  handleClickClose = () => {
-    this.setState({ isDeleteDialogOpen: false });
-  };
-
-  keywordInput = (classes, classNames) => {
-    return (
-      <Grid item xs={12} sm={3}>
-        <TextField
-          id="keyword"
-          label="Keyword"
-          className={classNames(classes.formField, classes.cssOutlinedInput)}
-          placeholder="First name, last name, tax number..."
-          InputLabelProps={{
-          classes: {
-          root: classes.formFieldInputLabel,
-          focused: classes.cssFocused,
-          },
-          }}
-          InputProps={{
-          classes: {
-          root: classes.formFieldInput,
-          notchedOutline: classes.cssOutlinedInput,
-          },
-          }}
-          // value="{this.state.name}"
-          // onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"/>
-      </Grid>
-    )
-  }
-
-  public render() {
-    const { classes, theme } = this.props;
-    const { 
-      items, 
-      isLoaded, 
-      error, 
-      isDeleteDialogOpen } = this.state;
-
-
-    const patients = (
-      items && items.length !== 0 ?
-        (items.map((item) => (
-          <Patients
-            info={item}
-          />
-        ))) :
-        <CircularProgress className={classes.progress} color="secondary" style={{ margin: '20px auto' }} />
-    )
-
-    return (
-      <div className={classes.root}>
-        <Grid container className={classes.gridContainer} justify='center' spacing={24}>
-          <Grid container item justify='center' spacing={24}>
-            
-            <Grid item xs={12}>
-              <Breadcrumbs aria-label="Breadcrumb" className={classes.breadCrumb}>
-                <MaterialLinkRouter color="secondary" component={LinkRouter} to="/dashboard">
-                  Home
-                </MaterialLinkRouter>
-                <Typography color="inherit">Patients</Typography>
-              </Breadcrumbs>
-            </Grid>
-
-            <Grid item xs={12} className={classes.patientActions}>
-              <Typography variant="inherit" className={classes.patientsTitle}>
-                PATIENTS
-              </Typography>
-              <Grid>
-                <Button color="inherit" onClick={this.handleClickOpen} classes={{ root: classes.button, label: classes.buttonLabel }}>
-                  <CancelIcon className={classes.buttonIcon} />
-                  Delete a patient
-              </Button>
-                <DeletePatientDialog isOpen={isDeleteDialogOpen} handleClickClose={this.handleClickClose}/>
-              </Grid>                  
-              <MaterialButtonRouter component={LinkRouter} to="/patientsDatabase/newPatient" color="inherit" classes={{ root: (classNames(classes.button, 'addButton')), label: classes.buttonLabel }}>
-                <AddIcon className={classes.buttonIcon} />
-                Record new patient
-              </MaterialButtonRouter>
-              <Button color="inherit" classes={{ root: (classNames(classes.button, 'mergeButton')), label: classes.buttonLabel }}>
-                <MergeIcon className={classes.buttonIcon} />
-                Merge double patients' registration
+            <div className={classes.root}>
+                <Grid container className={classes.gridContainer} justify='center' spacing={24}>
+                    <Grid container item justify='center' spacing={24}>
+                        <Grid item xs={12}>
+                            <Breadcrumbs aria-label="Breadcrumb" className={classes.breadCrumb}>
+                                <MaterialLinkRouter color="secondary" component={LinkRouter} to="/dashboard">
+                                    Home
+                                </MaterialLinkRouter>
+                                <Typography color="inherit">Patients</Typography>
+                            </Breadcrumbs>
+                        </Grid>
+                        <Grid item xs={12} className={classes.patientActions}>
+                            <Typography variant="inherit" className={classes.patientsTitle}>
+                                PATIENTS
+                            </Typography>
+                            <Grid>
+                                <Button color="inherit" onClick={this.handleClickOpen} classes={{ root: classes.button, label: classes.buttonLabel }}>
+                                    <CancelIcon className={classes.buttonIcon} />
+                                    Delete a patient
+                                </Button>
+                                <DeletePatientDialog isOpen={isDeleteDialogOpen} handleClickClose={this.handleClickClose}/>
+                            </Grid>                  
+                            <MaterialButtonRouter component={LinkRouter} to="/patientsDatabase/newPatient" color="inherit" classes={{ root: (classNames(classes.button, 'addButton')), label: classes.buttonLabel }}>
+                                <AddIcon className={classes.buttonIcon} />
+                                Record new patient
+                            </MaterialButtonRouter>
+                            <Button color="inherit" classes={{ root: (classNames(classes.button, 'mergeButton')), label: classes.buttonLabel }}>
+                                <MergeIcon className={classes.buttonIcon} />
+                                Merge double patients' registration
                             </Button>
-            </Grid>
-          </Grid>
+                        </Grid>
+                    </Grid>
 
-          <Grid container item justify='center' spacing={24}>
-            <Paper className={classes.paperFlat}>
-              <Grid container item spacing={24} className={classes.inputContainer}>
-                
-                <Grid item xs={12} style={{ display: 'flex' }}>
-                  <Typography variant="inherit" className={classes.findPatients}>
-                    FIND A PATIENT
-                </Typography>
-                  <Typography variant="inherit" className={classes.insertInfoPatients}>
-                    Insert the information of the patient
-                </Typography>
+                    <Grid container item justify='center' spacing={24}>
+                        <Paper className={classes.paperFlat}>
+                            <Grid container item spacing={24} className={classes.inputContainer}>
+                                <Grid item xs={12} style={{ display: 'flex' }}>
+                                    <Typography variant="inherit" className={classes.findPatients}>
+                                        FIND A PATIENT
+                                    </Typography>
+                                    <Typography variant="inherit" className={classes.insertInfoPatients}>
+                                        Insert the information of the patient
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                            <PatientBasicInfoForm extraInput={this.keywordInput}/>
+                        </Paper>
+                    </Grid>
+
+                    <Grid container item spacing={24} className={classes.filterContainer}>
+
+                        <Grid item xs={12} style={{ display: 'flex' }}>
+                            <Typography variant="inherit" className={classes.findPatients}>
+                                Which patient are you searching for?
+                            </Typography>
+                            <Typography variant="inherit" className={classes.insertInfoPatients}>
+                                Use the filter for a faster search
+                            </Typography>
+                        </Grid>
+
+                        <Divider className={classes.divider} />
+                        <Grid item xs={12} sm={3}>
+                            <FormControl variant="outlined" className={classNames(classes.formField, classes.formFieldSelect)}>
+                                <Select
+                                    className={classes.select}
+                                    // value={this.state.age}
+                                    // onChange={this.handleChange}
+                                    input={<OutlinedInput
+                                                placeholder="soma"
+                                                labelWidth={300} //{this.state.InputLabelRef}
+                                                name="filter"
+                                                id="filter"
+                                                enableSearch
+                                                // inputProps={{                          
+                                                classes={{
+                                                    // root: classes.formFieldSelectInput,
+                                                    input: classes.formFieldSelectInput}}
+                                    />}
+                                >
+                                    <MenuItem value={10}>Chronic Patient</MenuItem>
+                                    <MenuItem value={20}>Properly admission</MenuItem>
+                                    <MenuItem value={30}>Visited this month</MenuItem>
+                                    <MenuItem value={30}>Visited last month</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                    <Grid container item style={{ padding: '47px 0' }} spacing={24}>
+                        {patients}
+                    </Grid>
+                    <Grid item xs={12} sm={2} className={classes.loadMoreContainer}>
+                        <Button type="button" variant="outlined" color="inherit" classes={{ root: classes.button, label: classes.buttonLabel }}>
+                            Load more
+                        </Button>
+                    </Grid>
                 </Grid>
-
-              </Grid>
-              <PatientBasicInfoForm extraInput={this.keywordInput}/>              
-
-            </Paper>
-          </Grid>
-          <Grid container item spacing={24} className={classes.filterContainer}>
-            <Grid item xs={12} style={{ display: 'flex' }}>
-              <Typography variant="inherit" className={classes.findPatients}>
-                Which patient are you searching for?
-                </Typography>
-              <Typography variant="inherit" className={classes.insertInfoPatients}>
-                Use the filter for a faster search
-                </Typography>
-            </Grid>
-            <Divider className={classes.divider} />
-            <Grid item xs={12} sm={3}>
-              <FormControl variant="outlined"
-                className={classNames(classes.formField, classes.formFieldSelect)}>
-                <Select
-                  className={classes.select}
-                  // value={this.state.age}
-                  // onChange={this.handleChange}
-                  input={
-                    <OutlinedInput
-                      placeholder="soma"
-                      labelWidth={300} //{this.state.InputLabelRef}
-                      name="filter"
-                      id="filter"
-                      enableSearch
-                      // inputProps={{                          
-                      classes={{
-                        // root: classes.formFieldSelectInput,
-                        input: classes.formFieldSelectInput
-                      }}
-                    // }}
-                    />
-                  }
-                >
-                  <MenuItem value={10}>Chronic Patient</MenuItem>
-                  <MenuItem value={20}>Properly admission</MenuItem>
-                  <MenuItem value={30}>Visited this month</MenuItem>
-                  <MenuItem value={30}>Visited last month</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-
-          </Grid>
-          <Grid container item style={{ padding: '47px 0' }} spacing={24}>
-            {patients}
-          </Grid>
-          <Grid item xs={12} sm={2} className={classes.loadMoreContainer}>
-            <Button type="button" variant="outlined" color="inherit" classes={{ root: classes.button, label: classes.buttonLabel }}>
-              Load more
-            </Button>
-          </Grid>
-        </Grid>
-      </div >
-    );
-  }
+            </div>
+        );
+    }
 }
 
 
