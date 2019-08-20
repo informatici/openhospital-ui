3a6fcd47587b545b8a70ad7f2aeab2825c1eda9a
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-08-20 12:59:19 +0200

    feat: create redux store and apply it to Provider Main changes in /src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,19 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import './index.css';
 
+// redux imports
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root') as HTMLElement
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root') as HTMLElement
 );
-
 
 registerServiceWorker();
