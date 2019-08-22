3eacb24052799f217e37cc06079737f6ca2dc01c
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-08-22 15:45:23 +0200

    feat: apply immer to patientInDetails reducer

--- src/reducers/patientInDetails.tsx
+++ src/reducers/patientInDetails.tsx
@@ -1,15 +1,17 @@
+import produce from 'immer';
+
 import {
 	GET_PATIENT,
 	CLEAR_PATIENT_IN_DETAILS,
 } from '../actions/patientInDetails';
 
 export default function patients(state={}, action){
-	switch(action.type){
-		case GET_PATIENT :
-			return action.patient;
-		case CLEAR_PATIENT_IN_DETAILS :
-			return action.patient;
-		default :
-			return state;
-	}
+	return produce(state, draft => {
+		switch(action.type){
+			case GET_PATIENT :
+				return action.patient;
+			case CLEAR_PATIENT_IN_DETAILS :
+				return {};
+		}
+	})
 }
