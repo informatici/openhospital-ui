0ccad60aa9e1535b541bab47612811be583d8747
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-09-02 17:11:25 +0200

    feat: add type checking to patients. adjusments needed though

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -3,8 +3,10 @@ import patients from './patients';
 import patientInDetails from './patientInDetails';
 import loading from './loading';
 
-export default combineReducers({
+const rootReducer = combineReducers({
 	patientInDetails,
 	patients,
 	loading,
 })
+
+export type AppState = ReturnType<typeof rootReducer>
