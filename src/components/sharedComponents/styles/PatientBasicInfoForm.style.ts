56c35182f162e5b4e8e736275eb377f1bd03f284
Henrique de Almeida <henriquealmeida1492@gmail.com>
2020-01-18 09:21:51 +0100

    Resolve OP-142-form-field-animation (#7)
    * Fix login textField styles and login path
    * Refactor PatientBasicInfoForm
    * Adjust PatientBasicInfoForm fields style

--- src/components/sharedComponents/styles/PatientBasicInfoForm.style.ts
+++ src/components/sharedComponents/styles/PatientBasicInfoForm.style.ts
@@ -1,31 +1,33 @@
 import { createStyles, Theme } from "@material-ui/core/styles";
 
 const styles = (theme: Theme) =>
-	createStyles({
-		formField: {
-			width: '100%',
-		},
-		cssOutlinedInput: {
-			borderColor: `${theme.palette.primary.inputBorder} !important`,
-			'& legend': {
-				width: '0 !important'
-			}
-		},
-		formFieldInputLabel: {
-			transform: 'translate(14px, 14px) scale(1)',
-		},
-		cssFocused: {
-			'&$cssFocused': {
-				// color: 'green',
-				transform: 'translate(0px, -20px) scale(1)',
-			},
-		},
-		formFieldInput: {
-			padding: '6px 0',
-			'& input': {
-				padding: 6
-			}
-		},
-	});
+  createStyles({
+    formField: {
+      width: "100%",
+    },
+    cssOutlinedInput: {
+      borderColor: `${theme.palette.primary.inputBorder} !important`,
+      "& legend": {
+        width: "0 !important",
+      },
+    },
+    formFieldInputLabel: {
+      transform: "translate(14px, 19px) scale(1)",
+      backgroundColor: "white",
+      borderRadius: 2,
+      padding: "1px",
+    },
+    cssFocused: {
+      "&$cssFocused": {
+        transform: "translate(5px, -20px) scale(0.85)",
+      },
+    },
+    formFieldInput: {
+      padding: "6px 0",
+      "& input": {
+        padding: 6,
+      },
+    },
+  });
 
 export default styles;
