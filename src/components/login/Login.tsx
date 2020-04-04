3a3f1b46d2f2fb921650e2cca679cbc854c40ab2
almeida1492 <henriquealmeida1492@gmail.com>
2020-04-04 16:10:22 +0200

    OP-202 | Apply login.scss

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -35,63 +35,50 @@ const Login: FunctionComponent<Props> = ({ classes, successRoute }) => {
   };
 
   return (
-    <div className={classes.root}>
-      <div className={classes.gridContainer}>
-        <div className={classes.loginPanel}>
-          <Paper className={classNames(classes.paperFlat, classes.paper)}>
-            <div className={classes.gridContainer}>
-              <img src={OHlogo} alt="Open Hospital" className={classes.logo} />
+    <div className="login">
+      <div className="container login__background">
+        <div className="container login__panel col-md-8 col-sm-10 col-xs-11">
+          <div className="login__panel__content-grid center-xs col-md-8 col-sm-10 col-xs-11">
+            <div className="login__panel__content">
+              <img src={OHlogo} alt="Open Hospital" className="login__panel__logo" />
+              <div className="login__panel__form col-md-8 col-sm-9 col-xs-11">
+                <form onSubmit={formik.handleSubmit}>
+                  <TextField
+                    field={formik.getFieldProps("email")}
+                    label="Email"
+                    isValid={isValid("email")}
+                    errorText={getErrorText("email")}
+                    onBlur={formik.handleBlur}
+                  />
+                  <TextField
+                    field={formik.getFieldProps("password")}
+                    label="Password"
+                    type="Password"
+                    isValid={isValid("password")}
+                    errorText={getErrorText("password")}
+                    onBlur={formik.handleBlur}
+                  />
+                  <div className={classes.gridButtonContainer}>
+                    <Button
+                      type="submit"
+                      variant="outlined"
+                      color="inherit"
+                      classes={{ root: classes.button, label: classes.buttonLabel }}
+                    >
+                      ENTER
+                    </Button>
+                  </div>
+                  <div className={classes.forgotContainer}>
+                    <Link component="button" className={classes.forgotLink}>
+                      FORGOT PASSWORD?
+                    </Link>
+                  </div>
+                  &emsp;
+                </form>
+              </div>
             </div>
-            &emsp;
-            <Grid className={classes.loginForm} justify="center" spacing={24}>
-              <form onSubmit={formik.handleSubmit}>
-                <TextField
-                  field={formik.getFieldProps("email")}
-                  label="Email"
-                  isValid={isValid("email")}
-                  errorText={getErrorText("email")}
-                  onBlur={formik.handleBlur}
-                />
-                <TextField
-                  field={formik.getFieldProps("password")}
-                  label="Password"
-                  type="Password"
-                  isValid={isValid("password")}
-                  errorText={getErrorText("password")}
-                  onBlur={formik.handleBlur}
-                />
-                <div className={classes.gridButtonContainer}>
-                  <Button
-                    type="submit"
-                    variant="outlined"
-                    color="inherit"
-                    classes={{ root: classes.button, label: classes.buttonLabel }}
-                  >
-                    ENTER
-                  </Button>
-                </div>
-                <div className={classes.forgotContainer}>
-                  <Link component="button" className={classes.forgotLink}>
-                    FORGOT PASSWORD?
-                  </Link>
-                </div>
-              </form>
-              <Grid container className={classes.notRegisterContainer} spacing={24}>
-                &emsp;
-                <Divider className={classes.divider} />
-              </Grid>
-              <Grid container className={classes.notRegisterContainer} spacing={24}>
-                <Typography variant="inherit" className={classes.notRegisterLink}>
-                  If you have not registered yet,&nbsp;
-                </Typography>
-                <Link component="button" className={classes.notRegisterLink}>
-                  CLICK HERE
-                </Link>
-              </Grid>
-            </Grid>
-          </Paper>
+          </div>
         </div>
-        <SummaryBoard />
       </div>
     </div>
   );
