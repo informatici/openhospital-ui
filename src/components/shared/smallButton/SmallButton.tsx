6d6fc0781b47b6762bc03d8c3388f49ec7a734c4
almeida1492 <henriquealmeida1492@gmail.com>
2020-06-03 14:45:11 +0200

    Code polishing

--- src/components/shared/smallButton/SmallButton.tsx
+++ src/components/shared/smallButton/SmallButton.tsx
@@ -6,12 +6,7 @@ import { IProps } from "./types";
 const SmallButton: FunctionComponent<IProps> = ({ children, type }) => {
   return (
     <div>
-      <MaterialComponent
-        className="smallButton"
-        type={type}
-        // size="small"
-        disableElevation
-      >
+      <MaterialComponent className="smallButton" type={type} disableElevation>
         {children}
       </MaterialComponent>
     </div>
