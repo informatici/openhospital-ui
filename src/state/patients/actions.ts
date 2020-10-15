80d8ea51fab429b5060c8c7d24bf536d123abdda
almeida1492 <henriquealmeida1492@gmail.com>
2020-10-15 15:21:43 +0200

    OP-316 | Implement applyTokenMiddleware

--- src/state/patients/actions.ts
+++ src/state/patients/actions.ts
@@ -1,14 +1,21 @@
 import { Dispatch } from "redux";
 import { IAction } from "../types";
-import { PatientDTO, PatientControllerApi } from "../../generated";
+import {
+  PatientDTO,
+  PatientControllerApi,
+  Configuration,
+} from "../../generated";
 import {
   CREATE_PATIENT_LOADING,
   CREATE_PATIENT_SUCCESS,
   CREATE_PATIENT_FAIL,
   CREATE_PATIENT_RESET,
 } from "./consts";
+import { applyTokenMiddleware } from "../../libraries/apiUtils/applyTokenMiddleware";
 
-const patientControllerApi = new PatientControllerApi();
+const patientControllerApi = new PatientControllerApi(
+  new Configuration({ middleware: [applyTokenMiddleware] })
+);
 
 export const createPatient = (newPatient: PatientDTO) => (
   dispatch: Dispatch<IAction<null, {}>>
