e14a5a3a171c969349f1ade4354567ab4456c097
Alex Pezzini <alex.pezzini@intesys.it>
2020-10-29 14:35:13 +0100

    PatientDetailsActivity content placeholder

--- src/components/activities/patientDetailsActivity/tabsConfig.tsx
+++ src/components/activities/patientDetailsActivity/tabsConfig.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { TTabConfig } from "../../accessories/tabs/types";
 
 export const patientDetailTabs: TTabConfig = [
-  { label: "OPD", content: <span>Content 1</span> },
-  { label: "Summary", content: <span>Content 2</span> },
-  { label: "Triage", content: <span>Content 3</span> },
-  { label: "Admission", content: <span>Content 4</span> },
-  { label: "Therapy", content: <span>Content 5</span> },
-  { label: "Vaccination", content: <span>Content 6</span> },
-  { label: "Lab Exam", content: <span>Content 7</span> },
+  { label: "Summary", content: <div className="patientDetils__content__body_header">Summary</div> },
+  { label: "Triage", content: <div className="patientDetils__content__body_header">Triage</div> },
+  { label: "Admission", content: <div className="patientDetils__content__body_header">Admission</div> },
+  { label: "Therapy", content: <div className="patientDetils__content__body_header">Therapy</div> },
+  { label: "Vaccination", content: <div className="patientDetils__content__body_header">Vaccination</div> },
+  { label: "Lab Exam", content: <div className="patientDetils__content__body_header">Lab Exam</div> },
+  { label: "OPD", content: <div className="patientDetils__content__body_header">OPD</div> },
 ];
+
