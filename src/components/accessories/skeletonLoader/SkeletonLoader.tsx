62abf51af03e15e3c5195e75b16f2da0abb14b37
Alex Pezzini <alex.pezzini@intesys.it>
2020-11-23 10:23:26 +0100

    OP-342 | Add some placeholder content to PatientDetailActivity

---
+++ src/components/accessories/skeletonLoader/SkeletonLoader.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+import React, { Fragment, FunctionComponent } from "react";
+import Skeleton from '@material-ui/lab/Skeleton';
+
+const SkeletonLoader: FunctionComponent = () => {
+  return (
+    <Fragment>
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" variant="rect" height={200} style={{ margin: '5px 0px' }} />
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+      <Skeleton animation="wave" />
+    </Fragment>
+  );
+};
+
+export default SkeletonLoader;
