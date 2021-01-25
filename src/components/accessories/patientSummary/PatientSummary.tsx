ba6e830ed828223fd420d278bcdd6f7f863d1b26
Alex Pezzini <alex.pezzini@intesys.it>
2021-01-25 15:50:35 +0100

    OP-413 | Fix tabs style

--- src/components/accessories/patientSummary/PatientSummary.tsx
+++ src/components/accessories/patientSummary/PatientSummary.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { patientSummaryTabs } from "./tabsConfig";
 import Table from '../table/Table';
 import Tabs from '../tabs/Tabs';
+import "./styles.scss";
 
 const data = [
   { date: "21/12/2021", type: "Pharmacologic treatment", result: "Result (1)", note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
