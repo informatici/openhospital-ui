127548d6c6a6604d83880093b9519dfc3d848a1e
Alex Pezzini <alex.pezzini@intesys.it>
2021-01-28 14:38:35 +0100

    New patient menu + favicon

--- src/components/activities/patientDetailsActivity/tabsConfig.tsx
+++ src/components/activities/patientDetailsActivity/tabsConfig.tsx
@@ -7,12 +7,10 @@ import PatientSummary from "../../accessories/patientSummary/PatientSummary";
 import PatientDetailsContent from '../patientDetailsActivityContent/PatientDetailsActivityContent';
 
 export const patientDetailTabs: TTabConfig = [
-  { label: "ODP", content: <PatientDetailsContent title="ODP" content={<PatientOPD />} /> },
   { label: "Summary", content: <PatientDetailsContent title="Summary" content={<PatientSummary />} /> },
+  { label: "OPD", content: <PatientDetailsContent title="OPD" content={<PatientOPD />} /> },
   { label: "Triage", content: <PatientDetailsContent title="Triage" content={<PatientTriage />} /> },
-  { label: "Admission", content: <PatientDetailsContent title="Admission" content={<SkeletonLoader />} /> },
   { label: "Therapy", content: <PatientDetailsContent title="Therapy" content={<SkeletonLoader />} /> },
-  { label: "Vaccination", content: <PatientDetailsContent title="Vaccination" content={<SkeletonLoader />} /> },
-  { label: "Lab Exam", content: <PatientDetailsContent title="Lab Exam" content={<SkeletonLoader />} /> },
+  { label: "Booking", content: <PatientDetailsContent title="Booking" content={<SkeletonLoader />} /> },
 ];
 
