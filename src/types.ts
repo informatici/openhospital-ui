ae50e66bef0083ce958e25cb086a27524aa3e384
Alessandro Falezza <alessandro.falezza@intesys.it>
2021-04-02 18:37:18 +0200

    OP-363 | Add permission type

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,33 @@ export interface IState {
   main: IMainState;
   patients: IPatientsState;
 }
+
+export type TPermission =
+  | "odp.read"
+  | "odp.create"
+  | "odp.update"
+  | "odp.delete"
+  | "summary.read"
+  | "summary.create"
+  | "summary.update"
+  | "summary.delete"
+  | "examination.read"
+  | "examination.create"
+  | "examination.update"
+  | "examination.delete"
+  | "admission.read"
+  | "admission.create"
+  | "admission.update"
+  | "admission.delete"
+  | "therapy.read"
+  | "therapy.create"
+  | "therapy.update"
+  | "therapy.delete"
+  | "vaccine.read"
+  | "vaccine.create"
+  | "vaccine.update"
+  | "vaccine.delete"
+  | "exam.read"
+  | "exam.create"
+  | "exam.update"
+  | "exam.delete";
