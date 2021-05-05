9311ec26fcce04caea57eba92f0f6209d00a3e55
Alessandro Falezza <alessandro.falezza@intesys.it>
2021-05-05 10:49:58 +0200

    Fix authentication issue in cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -34,6 +34,39 @@ Cypress.Commands.add("authenticate", (START_PATH) => {
         "auth",
         `{"displayName":"John Doe","token":"1qrj12fcxu3a21d21pjvba6g1"}`
       );
+      w.sessionStorage.setItem("permission", {
+        permission: [
+          "odp.read",
+          "odp.create",
+          "odp.update",
+          "odp.delete",
+          "summary.read",
+          "summary.create",
+          "summary.update",
+          "summary.delete",
+          "examination.read",
+          "examination.create",
+          "examination.update",
+          "examination.delete",
+          "admission.read",
+          "admission.create",
+          "admission.update",
+          "admission.delete",
+          "therapy.read",
+          "therapy.create",
+          "therapy.update",
+          "therapy.delete",
+          "vaccine.read",
+          "vaccine.create",
+          "vaccine.update",
+          "vaccine.delete",
+          "exam.read",
+          "exam.create",
+          "exam.update",
+          "exam.delete",
+        ],
+        userName: "admin",
+      });
     },
   });
 });
