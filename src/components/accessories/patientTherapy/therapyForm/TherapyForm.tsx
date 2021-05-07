a00bfe48b7812997d879a5251f9d4574ab31c959
almeida1492 <henriquealmeida1492@gmail.com>
2021-05-07 11:28:00 +0200

    Add dummy formik field to TherapyForm

--- src/components/accessories/patientTherapy/therapyForm/TherapyForm.tsx
+++ src/components/accessories/patientTherapy/therapyForm/TherapyForm.tsx
@@ -1,76 +1,52 @@
 import { TextField } from "@material-ui/core";
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
 
-const TherapyForm : FC = (props) => {
-    return (
-        <>
-          <div className="patientTherapyForm">
-            <form
-              className="patientTherapyForm__form"
-            >
-                <div className="row start-sm center-xs">
-                
-        
-                    <div className="patientTherapyForm__item">
-                        <TextField
-                            label="quantity"
-                            type="number"
-                        />
-                    </div>
-                </div>
-
-                <div className="row start-sm center-xs">
-                    <div className="patientTherapyForm__item">
-                        <input
-                            name="one"
-                            type="radio"
-                        />One
-                        <input
-                            name="two"
-                            type="radio"
-                        />Two
-                        <input
-                            name="three"
-                            type="radio"
-                        />Three
-                        <input
-                            name="four"
-                            type="radio"
-                        />Four
-                    </div>
-                    <div className="patientTherapyForm__item">
-                        
-                        <TextField
-                            label="days"
-                            type="text"
-                        />
-                    </div>
-                    <div className="patientTherapyForm__item">
-                        
-                        <TextField
-                            label="days"
-                            type="number"
-                        />
-                    </div>
-        
-                    <div className="patientTherapyForm__item">
-                        <TextField
-                            label="weeks"
-                            type="number"
-                        />
-                    </div>
-                    <div className="patientTherapyForm__item">
-                        <TextField
-                            label="months"
-                            type="number"
-                        />
-                    </div>
-                </div>   
-    
-            </form>
+const TherapyForm: FC = (props) => {
+  const dummyField = {
+    value: "dummyValue",
+    name: "dummyName",
+    onChange: (e: ChangeEvent<any>) => console.log(e),
+    onBlur: (e: ChangeEvent<any>) => console.log(e),
+  };
+  return (
+    <>
+      <div className="patientTherapyForm">
+        <form className="patientTherapyForm__form">
+          <div className="row start-sm center-xs">
+            <div className="patientTherapyForm__item">
+              <TextField label="quantity" type="number" />
+            </div>
           </div>
-        </>
-      );
-}
 
-export default TherapyForm
+          <div className="row start-sm center-xs">
+            <div className="patientTherapyForm__item">
+              <input name="one" type="radio" />
+              One
+              <input name="two" type="radio" />
+              Two
+              <input name="three" type="radio" />
+              Three
+              <input name="four" type="radio" />
+              Four
+            </div>
+            <div className="patientTherapyForm__item">
+              <TextField label="days" type="text" />
+            </div>
+            <div className="patientTherapyForm__item">
+              <TextField label="days" type="number" />
+            </div>
+
+            <div className="patientTherapyForm__item">
+              <TextField label="weeks" type="number" />
+            </div>
+            <div className="patientTherapyForm__item">
+              <TextField label="months" type="number" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default TherapyForm;
