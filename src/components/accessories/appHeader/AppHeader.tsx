2672141c46c1b1c5007b542fc36103c10368ca86
Alex Pezzini <alex.pezzini@intesys.it>
2021-06-25 18:04:43 +0200

    Restyle header userInfo + minor fix

--- src/components/accessories/appHeader/AppHeader.tsx
+++ src/components/accessories/appHeader/AppHeader.tsx
@@ -1,5 +1,6 @@
 import { Tooltip, Typography } from "@material-ui/core";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import { Trans } from "react-i18next";
 import HomeIcon from "@material-ui/icons/Home";
 import LangSwitcher from "../langSwitcher/LangSwitcher";
 import NavigateBefore from "@material-ui/icons/NavigateBefore";
@@ -20,7 +21,6 @@ import warningIcon from "../../../assets/warning-icon.png";
 const AppHeader: FunctionComponent<TProps> = ({
   breadcrumbMap,
   setLogoutThunk,
-  status,
 }) => {
   const keys = Object.keys(breadcrumbMap);
   const trailEdgeKey = keys.pop();
@@ -45,66 +45,69 @@ const AppHeader: FunctionComponent<TProps> = ({
   const history = useHistory();
   return (
     <div className={classNames("appHeader", { open_menu: isOpen })}>
-      <div className="appHeader__background">
-        <div className="appHeader__identifier">
-          <div className="appHeader__identifier__logo">
-            <img src={logo} alt="Open Hospital" height="45px" />
-          </div>
-          <div
-            onClick={() => history.push(breadcrumbMap[keys.pop() || "/"])}
-            className={classNames("appHeader__navigate_before", {
-              hidden: trailEdgeKey === "Dashboard",
-            })}
-          >
-            <NavigateBefore fontSize="large" style={{ color: "#fc1812" }} />
-          </div>
-          <div className="appHeader__identified__main">
-            <div className="appHeader__identified__main__headline">
-              Princeton-Plainsboro Teaching Hospital
-            </div>
-            <Breadcrumbs>
-              <div className="appHeader__home_icon">
-                <HomeIcon fontSize="small" style={{ color: "#fff" }} />
-              </div>
-              {keys.map((key, index) => (
-                <Link key={index} to={breadcrumbMap[key]}>
-                  <Typography color="textPrimary">{key}</Typography>
-                </Link>
-              ))}
-              <Typography color="textPrimary">{trailEdgeKey}</Typography>
-            </Breadcrumbs>
-          </div>
-          <div
-            className="appHeader__identified__trigger"
-            onClick={() => openMenu(!isOpen)}
-          >
-            <div className="trigger_x"></div>
-            <div className="trigger_y"></div>
-            <div className="trigger_z"></div>
-          </div>
+      <div className="appHeader__top">
+        <div className="appHeader__nav_lang_switcher">{<LangSwitcher />}</div>
+        <div className="userInfo__toolbar">
+          <span>
+            <Trans
+              i18nKey="dashboard.welcomename"
+              values={{ name: username }}
+            />
+          </span>
+          <Tooltip title={t("login.signout")!} aria-label="sign out">
+            <ExitToAppIcon
+              className="userInfo__toolbar_icon"
+              id="signout_icon"
+              onClick={() => setOpenLogoutConfirmation(true)}
+            />
+          </Tooltip>
         </div>
-        <div className="appHeader__nav">
-          <div className="appHeader__nav_top_right">
-            <div className="appHeader__nav_lang_switcher">
-              {<LangSwitcher />}
+      </div>
+      <div className="appHeader__bottom">
+        <div className="appHeader__background">
+          <div className="appHeader__identifier">
+            <div className="appHeader__identifier__logo">
+              <img src={logo} alt="Open Hospital" height="45px" />
             </div>
-            <div className="userInfo__toolbar">
-              <span>
-                Welcome, <strong>{username}</strong>
-              </span>
-              <Tooltip title={t("login.signout")!} aria-label="sign out">
-                <ExitToAppIcon
-                  className="userInfo__toolbar_icon"
-                  id="signout_icon"
-                  onClick={() => setOpenLogoutConfirmation(true)}
-                />
-              </Tooltip>
+            <div
+              onClick={() => history.push(breadcrumbMap[keys.pop() || "/"])}
+              className={classNames("appHeader__navigate_before", {
+                hidden: trailEdgeKey === "Dashboard",
+              })}
+            >
+              <NavigateBefore fontSize="large" style={{ color: "#fc1812" }} />
+            </div>
+            <div className="appHeader__identified__main">
+              <div className="appHeader__identified__main__headline">
+                Princeton-Plainsboro Teaching Hospital
+              </div>
+              <Breadcrumbs>
+                <div className="appHeader__home_icon">
+                  <HomeIcon fontSize="small" style={{ color: "#fff" }} />
+                </div>
+                {keys.map((key, index) => (
+                  <Link key={index} to={breadcrumbMap[key]}>
+                    <Typography color="textPrimary">{key}</Typography>
+                  </Link>
+                ))}
+                <Typography color="textPrimary">{trailEdgeKey}</Typography>
+              </Breadcrumbs>
+            </div>
+            <div
+              className="appHeader__identified__trigger"
+              onClick={() => openMenu(!isOpen)}
+            >
+              <div className="trigger_x"></div>
+              <div className="trigger_y"></div>
+              <div className="trigger_z"></div>
             </div>
           </div>
-          <div className="appHeader__nav_items">
-            <div className="appHeader__nav__item">{t("nav.pharmacy")}</div>
-            <div className="appHeader__nav__item">{t("nav.ward")}</div>
-            <div className="appHeader__nav__item">{t("nav.billing")}</div>
+          <div className="appHeader__nav">
+            <div className="appHeader__nav_items">
+              <div className="appHeader__nav__item">{t("nav.pharmacy")}</div>
+              <div className="appHeader__nav__item">{t("nav.ward")}</div>
+              <div className="appHeader__nav__item">{t("nav.billing")}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -118,7 +121,6 @@ const AppHeader: FunctionComponent<TProps> = ({
         handlePrimaryButtonClick={handleLogout}
         handleSecondaryButtonClick={() => setOpenLogoutConfirmation(false)}
       />
-      ;
     </div>
   );
 };
