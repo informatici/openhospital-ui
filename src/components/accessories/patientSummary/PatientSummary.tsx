1d9fe48863da2f304332f427c71c3e0bccd44876
Alex Pezzini <alex.pezzini@intesys.it>
2021-06-25 17:15:44 +0200

    Fix

--- src/components/accessories/patientSummary/PatientSummary.tsx
+++ src/components/accessories/patientSummary/PatientSummary.tsx
@@ -1,15 +1,14 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 import { patientSummaryTabs } from "./tabsConfig";
-import Tabs from '../tabs/Tabs';
+import Tabs from "../tabs/Tabs";
 import "./styles.scss";
 
 const PatientSummary: FunctionComponent = () => {
-
   return (
     <div className="patientSummary">
       <Tabs config={patientSummaryTabs} />
     </div>
   );
-}
+};
 
 export default PatientSummary;
