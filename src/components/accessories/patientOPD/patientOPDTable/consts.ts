27515a2aab84ea6dc7aa35a86ddc0bc6c162840a
tsognong <44397824+tsognong@users.noreply.github.com>
2021-07-15 17:41:44 +0100

    merge corrections

--- src/components/accessories/patientOPD/patientOPDTable/consts.ts
+++
@@ -1,9 +0,0 @@
-export const header = ["date"];
-export const label = {
-  date: "Date",
-  disease: "Diagnostic 1",
-  disease2: "Diagnostic 2",
-  disease3: "Diagnostic 3",
-  note: "Note",
-};
-export const order = ["date"];
