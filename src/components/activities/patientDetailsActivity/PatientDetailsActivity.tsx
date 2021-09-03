b67c4996a97c2f8edde5b32bd906a82482d714ad
tsognong <44397824+tsognong@users.noreply.github.com>
2021-09-03 18:44:24 +0100

    correct merge

--- src/components/activities/patientDetailsActivity/PatientDetailsActivity.tsx
+++ src/components/activities/patientDetailsActivity/PatientDetailsActivity.tsx
@@ -77,8 +77,8 @@ const PatientDetailsActivity: FunctionComponent<TProps> = ({
     useState<TActivityTransitionState>("IDLE");
   const [isOpen, setIsOpen] = useState(false);
   const [expanded, setExpanded] = useState<string | false>(false);
-  const [userSection, setUserSection] = useState<IUserSection>("admissions");
-  const [defaultRoute, setDefaultRoute] = useState("/admission");
+  const [userSection, setUserSection] = useState<IUserSection>("clinic");
+  const [defaultRoute, setDefaultRoute] = useState("/summary");
 
   const admissionsConfig: TTabConfig = [
     {
