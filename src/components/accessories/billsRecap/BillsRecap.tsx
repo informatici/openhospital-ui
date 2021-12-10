27ed9649155c983ff4498426feec00811f3999f6
tsognong <44397824+tsognong@users.noreply.github.com>
2021-12-10 13:28:34 +0100

    add pie charts

--- src/components/accessories/billsRecap/BillsRecap.tsx
+++ src/components/accessories/billsRecap/BillsRecap.tsx
@@ -1,34 +1,43 @@
-import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
-import {
-  AutorenewRounded,
-  BarChart,
-  CalendarToday,
-  ChatRounded,
-  CheckCircleOutline,
-  DateRange,
-  Person,
-  ViewWeek,
-} from "@material-ui/icons";
 import moment from "moment";
-import { Chart } from "react-charts";
-import React, { FC, useEffect, useMemo, useState } from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { FullBillDTO } from "../../../generated";
-import { currencyFormat } from "../../../libraries/formatUtils/currencyFormatting";
 import { searchBills } from "../../../state/bills/actions";
 import { TUserCredentials } from "../../../state/main/types";
 import { IState } from "../../../types";
 import { IBillSummary } from "../../activities/billingActivity/types";
 import { TFilterValues } from "../billTable/types";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar, Doughnut } from "react-chartjs-2";
 
 import { computeBillSummary } from "./config";
 
 import "./styles.scss";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 export const BillsRecap: FC = () => {
   const { t } = useTranslation();
   const [summary, billSummaryChange] = useState({} as IBillSummary);
   const dispatch = useDispatch();
+
   const data = useSelector<IState, FullBillDTO[]>((state) => {
     return state.bills.searchBills.data ?? [];
   });
@@ -54,266 +63,114 @@ export const BillsRecap: FC = () => {
     dispatch(searchBills(filter as TFilterValues));
   }, [dispatch]);
 
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: "ordinal", position: "bottom" },
-      { position: "left", type: "linear", stacked: true },
-    ],
-    []
+  const getOptions = (title: string) => {
+    return {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top" as const,
+        },
+        title: {
+          display: true,
+          text: title,
+        },
+      },
+    };
+  };
+
+  const getDataFromObject = useCallback(
+    (
+      obj: string,
+      label: string,
+      bgColor: string = "rgba(255, 99, 132, 0.5)"
+    ) => {
+      return {
+        labels: (summary as any)[obj] ? Object.keys((summary as any)[obj]) : [],
+        datasets: [
+          {
+            data: (summary as any)[obj]
+              ? Object.values((summary as any)[obj])
+              : [],
+            backgroundColor: bgColor,
+            label: label,
+          },
+        ],
+      };
+    },
+    [summary]
   );
 
-  const series = useMemo(
-    () => ({
-      type: "bar",
-    }),
-    []
+  const getDataFromTwoObjects = useCallback(
+    (obj1: string, obj2: string) => {
+      return {
+        labels: ["Total Payment", "Total Pending"],
+        datasets: [
+          {
+            data:
+              (summary as any)[obj1] && (summary as any)[obj2]
+                ? [(summary as any)[obj1], (summary as any)[obj2]]
+                : [],
+            label: "# Of bills",
+            backgroundColor: ["#B21F00", "#C9DE00"],
+            hoverBackgroundColor: ["#501800", "#4B5000"],
+          },
+        ],
+      };
+    },
+    [summary]
   );
-
-  const data1 = useMemo(() => {
-    return [
-      {
-        data: summary.bestSellingByQuantity
-          ? Object.entries(summary.bestSellingByQuantity)
-          : [],
-        dataType: "ordinal",
-      },
-    ];
-  }, [summary.bestSellingByQuantity]);
-
-  const data2 = useMemo(() => {
-    return [
-      {
-        data: summary.bestSellingByOccurence
-          ? Object.entries(summary.bestSellingByOccurence)
-          : [],
-      },
-    ];
-  }, [summary.bestSellingByOccurence]);
-
-  const data3 = useMemo(() => {
-    return [
-      {
-        data: summary.bestPatientsByPayments
-          ? Object.entries(summary.bestPatientsByPayments)
-          : [],
-      },
-    ];
-  }, [summary.bestPatientsByPayments]);
-
-  const data4 = useMemo(() => {
-    return [
-      {
-        data: summary.mostIndebtedPatients
-          ? Object.entries(summary.mostIndebtedPatients)
-          : [],
-      },
-    ];
-  }, [summary.mostIndebtedPatients]);
 
   return (
     <div className="bills__recap">
       <div className="bills__recap__content">
-        <Card className="item__card">
-          <CardHeader
-            title={`${t("bill.today")} (${moment().format("DD/MM/YYYY")})`}
-            avatar={<CalendarToday />}
+        <div className="info__area">
+          <Doughnut
+            data={getDataFromTwoObjects("dailyRevenue", "dailyDebt")}
+            options={getOptions(
+              `${t("bill.today")} (${moment().format("DD/MM/YYYY")})`
+            )}
           />
-          <CardContent>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <CheckCircleOutline style={{ fill: "green" }} />
-              {summary.dailyRevenue
-                ? currencyFormat(summary.dailyRevenue)
-                : "-"}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <AutorenewRounded style={{ fill: "red" }} />
-              {summary.dailyDebt ? currencyFormat(summary.dailyDebt) : "-"}
-            </Typography>
-          </CardContent>
-        </Card>
+          <Doughnut
+            data={getDataFromTwoObjects("weeklyRevenue", "weeklyDebt")}
+            options={getOptions(`${t("bill.week")}`)}
+          />
+          <Doughnut
+            data={getDataFromTwoObjects("monthlyRevenue", "monthlyDebt")}
+            options={getOptions(
+              `${t("bill.month")} (${moment().format("MMMM")})`
+            )}
+          />
+          <Doughnut
+            data={getDataFromTwoObjects("annualRevenue", "annualDebt")}
+            options={getOptions(
+              `${t("bill.year")} (${moment().format("YYYY")})`
+            )}
+          />
+        </div>
+        <div className="chart__area">
+          <Bar
+            options={getOptions("Best products By quantity")}
+            data={getDataFromObject("bestSellingByQuantity", "Total Quantity")}
+          />
 
-        <Card className="item__card">
-          <CardHeader title={`${t("bill.week")}`} avatar={<ViewWeek />} />
-          <CardContent>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <CheckCircleOutline style={{ fill: "green" }} />
-              {summary.weeklyRevenue
-                ? currencyFormat(summary.weeklyRevenue)
-                : "-"}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <AutorenewRounded style={{ fill: "red" }} />
-              {summary.weeklyDebt ? currencyFormat(summary.weeklyDebt) : "-"}
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className="item__card">
-          <CardHeader
-            title={`${t("bill.month")} (${moment().format("MMMM")})`}
-            avatar={<DateRange />}
+          <Bar
+            options={getOptions("Best products By occurence")}
+            data={getDataFromObject(
+              "bestSellingByOccurence",
+              "Total Occuence",
+              "rgba(53, 162, 235, 0.5)"
+            )}
           />
-          <CardContent>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <CheckCircleOutline style={{ fill: "green" }} />
-              {summary.monthlyRevenue
-                ? currencyFormat(summary.monthlyRevenue)
-                : "-"}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <AutorenewRounded style={{ fill: "red" }} />
-              {summary.monthlyDebt ? currencyFormat(summary.monthlyDebt) : "-"}
-            </Typography>
-          </CardContent>
-        </Card>
 
-        <Card className="item__card">
-          <CardHeader
-            title={`${t("bill.year")} (${moment().format("YYYY")})`}
-            avatar={<DateRange />}
+          <Bar
+            options={getOptions("Best patients")}
+            data={getDataFromObject("bestPatientsByPayments", "Total payments")}
           />
-          <CardContent>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <CheckCircleOutline style={{ fill: "green" }} />
-              {summary.annualRevenue
-                ? currencyFormat(summary.annualRevenue)
-                : "-"}
-            </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <AutorenewRounded style={{ fill: "red" }} />
-              {summary.annualDebt ? currencyFormat(summary.annualDebt) : "-"}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card className="item__card">
-          <CardHeader title={t("bill.user")} avatar={<Person />} />
-          <CardContent className="item__card__content">
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <CheckCircleOutline style={{ fill: "green" }} />
-              {summary.currentUserCashIn
-                ? currencyFormat(summary.currentUserCashIn)
-                : "-"}
-            </Typography>
-
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-              className="item__value"
-            >
-              <AutorenewRounded style={{ fill: "red" }} />
-              {summary.currentUserDebt
-                ? currencyFormat(summary.currentUserDebt)
-                : "-"}
-            </Typography>
-          </CardContent>
-        </Card>
-        <Card className="item__card">
-          <CardHeader
-            title={t("bill.bestsellingbyqty")}
-            avatar={<BarChart />}
+          <Bar
+            options={getOptions("Most Indebt patients")}
+            data={getDataFromObject("mostIndebtedPatients", "Total balance")}
           />
-          <CardContent className="item__card__content">
-            <div
-              style={{
-                width: "100%",
-                minHeight: "300px",
-              }}
-            >
-              <Chart data={data1} axes={axes} series={series} />
-            </div>
-          </CardContent>
-        </Card>
-        <Card className="item__card">
-          <CardHeader
-            title={t("bill.bestsellingbyoccurence")}
-            avatar={<BarChart />}
-          />
-          <CardContent className="item__card__content">
-            <div
-              style={{
-                width: "100%",
-                minHeight: "400px",
-              }}
-            >
-              <Chart data={data2} axes={axes} series={series} />
-            </div>
-          </CardContent>
-        </Card>
-        <Card className="item__card">
-          <CardHeader title={t("bill.bestPatients")} avatar={<BarChart />} />
-          <CardContent className="item__card__content">
-            <div
-              style={{
-                width: "100%",
-                minHeight: "400px",
-              }}
-            >
-              <Chart data={data3} axes={axes} series={series} />
-            </div>
-          </CardContent>
-        </Card>
-        <Card className="item__card">
-          <CardHeader
-            title={t("bill.mostIndebtPatients")}
-            avatar={<BarChart />}
-          />
-          <CardContent className="item__card__content">
-            <div
-              style={{
-                width: "100%",
-                minHeight: "400px",
-              }}
-            >
-              <Chart data={data4} axes={axes} series={series} />
-            </div>
-          </CardContent>
-        </Card>
+        </div>
       </div>
     </div>
   );
