1ef6d1f1005f15142d7f9e0a1067b25b346c684f
tsognong <44397824+tsognong@users.noreply.github.com>
2021-12-13 13:37:09 +0100

    few changes

--- src/components/accessories/billsRecap/BillsRecap.tsx
+++ src/components/accessories/billsRecap/BillsRecap.tsx
@@ -1,4 +1,4 @@
-import moment, { months } from "moment";
+import moment from "moment";
 import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
