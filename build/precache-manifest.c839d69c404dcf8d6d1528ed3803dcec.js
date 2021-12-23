b673f1f4455a5b28ed6256f16a08bd6e8e29762b
alex.pezzini <alex.pezzini@intesys.it>
2021-12-23 18:22:02 +0100

    New build

---
+++ build/precache-manifest.c839d69c404dcf8d6d1528ed3803dcec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4ebdf48c5cc2fae4ba1746b80793d6",
+    "url": "/openhospital-ui/index.html"
+  },
+  {
+    "revision": "2df5064e5079b6d56af7",
+    "url": "/openhospital-ui/static/css/main.9e46600a.chunk.css"
+  },
+  {
+    "revision": "8287022b891b91e7772e",
+    "url": "/openhospital-ui/static/js/2.fca9c732.chunk.js"
+  },
+  {
+    "revision": "a11378f4613de587e49b4fdd6c8f6e1a",
+    "url": "/openhospital-ui/static/js/2.fca9c732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df5064e5079b6d56af7",
+    "url": "/openhospital-ui/static/js/main.8bd8605b.chunk.js"
+  },
+  {
+    "revision": "c12a7ec583034f96a0b9",
+    "url": "/openhospital-ui/static/js/runtime-main.a861650c.js"
+  },
+  {
+    "revision": "193ed5ff4269c0b20b6a1043b6fa32c3",
+    "url": "/openhospital-ui/static/media/arrow-w.193ed5ff.svg"
+  },
+  {
+    "revision": "15d8fc8463a70d1c0c6d89e039528932",
+    "url": "/openhospital-ui/static/media/gender-female.15d8fc84.svg"
+  },
+  {
+    "revision": "d281d8c57295998c6f96552817cdb437",
+    "url": "/openhospital-ui/static/media/gender-male.d281d8c5.svg"
+  },
+  {
+    "revision": "2feb98122aeaecb90c2e144ad2bd4337",
+    "url": "/openhospital-ui/static/media/logo-color.2feb9812.svg"
+  },
+  {
+    "revision": "3d7e2682612c0aefa64868fd263f24f9",
+    "url": "/openhospital-ui/static/media/tax.3d7e2682.svg"
+  }
+]);
