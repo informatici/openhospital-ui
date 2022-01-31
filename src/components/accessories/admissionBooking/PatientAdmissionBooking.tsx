f883180b843cc96a7f83eb98ebc47d7094d701fb
Nicola Schiavon <nicola.schiavon@intesys.it>
2022-01-31 18:03:44 +0100

    added error message placeholder into admission bookings section

--- src/components/accessories/admissionBooking/PatientAdmissionBooking.tsx
+++ src/components/accessories/admissionBooking/PatientAdmissionBooking.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { IState } from "../../../types";
+import InfoBox from "../infoBox/InfoBox";
 
 const PatientAdmissionBooking: FC = () => {
   const status = useSelector<IState, any>((state: IState) => state.admissionBookings.admissionBookingsByPatientId.status);
-  console.log("STATUS", status);
+
   return (
     <div className="patientBooking">
-      Booking
+      {status === "FAIL" && <InfoBox type="error" message={"Something went wrong"} />}
     </div>
   );
 };
