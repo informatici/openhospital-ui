5d380be20957a9d81c5d55868240b69e16cc9dab
alex.pezzini <alex.pezzini@intesys.it>
2022-02-02 12:14:40 +0100

    Fix pipeline

--- src/components/activities/patientDetailsActivityContent/PatientDetailsActivityContent.tsx
+++ src/components/activities/patientDetailsActivityContent/PatientDetailsActivityContent.tsx
@@ -12,7 +12,7 @@ const PatientDetailsActivityContent: FunctionComponent<IProps> = ({
       <div className="patientDetils__content_header">
         <h3>{title}</h3>
       </div>
-      <div className="patientDetils__content_body1">
+      <div className="patientDetils__content_body">
         <Content />
       </div>
     </Fragment>
