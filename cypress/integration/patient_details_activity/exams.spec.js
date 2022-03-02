c603174ea65cc10ad10a289deb4380f22033948d
almeida1492 <henriquealmeida1492@gmail.com>
2022-03-02 10:57:46 +0100

    test(OP-815): Add exams.spec test cases

--- cypress/integration/patient_details_activity/exams.spec.js
+++ cypress/integration/patient_details_activity/exams.spec.js
@@ -1,4 +1,6 @@
-const START_PATH = "http://localhost:3000/details/1";
+const API_BASE_PATH = Cypress.env("API_BASE_PATH");
+const HOSTNAME = Cypress.env("HOSTNAME");
+const START_PATH = `${HOSTNAME}/details/1`;
 
 describe.skip("Patient Details / Exams", () => {
   before(() => {
@@ -13,14 +15,37 @@ describe.skip("Patient Details / Exams", () => {
     cy.get("[class='patientDetails__main_menu']").contains("Exams:").click();
   });
 
-  it("should make it possible for the use to fill out the form to add a new exam", () => {
+  it("should make it possible for the user to fill out the form to create a new exam", () => {
     cy.get("[id='date']").focus().type("02022022").blur();
     cy.get("[id='exam']").focus().type("1.3 Differential").blur();
     cy.get("[id='material']").focus().type("angal.lab.blood").blur();
     cy.get("[id='panel1bh-header']").click();
     cy.get("[class='PrivateSwitchBase-input-8']").first().click();
     cy.get("[id='note']").focus().type("note").blur();
+  });
+
+  it("should display an error dialog if the exam creation call fails", () => {
+    cy.intercept("POST", `${API_BASE_PATH}/laboratories`, {
+      statusCode: 400,
+    }).as("postLaboratories");
 
     cy.get("[class='submit_button']").click();
+
+    cy.wait("@postLaboratories");
+
+    cy.get("div.infoBox").should("have.class", "error");
+  });
+
+  it("should display a dialog confirming the exam creation when the call is successful", () => {
+    cy.intercept("POST", `${API_BASE_PATH}/laboratories`, {
+      statusCode: 201,
+    }).as("postLaboratories");
+
+    cy.get("[class='submit_button']").click();
+
+    cy.wait("@postLaboratories");
+
+    cy.get("div.dialog__title").contains("Patient exam added");
+    cy.get("div.return_button").click();
   });
 });
