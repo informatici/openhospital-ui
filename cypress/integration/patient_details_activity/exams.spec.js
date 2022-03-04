cbdaea9151d649b1949de3d89308bb194bc978ae
almeida1492 <henriquealmeida1492@gmail.com>
2022-03-04 09:59:28 +0100

    test(OP-815): Update exams.spec

--- cypress/integration/patient_details_activity/exams.spec.js
+++ cypress/integration/patient_details_activity/exams.spec.js
@@ -19,8 +19,7 @@ describe.skip("Patient Details / Exams", () => {
     cy.get("[id='date']").focus().type("02022022").blur();
     cy.get("[id='exam']").focus().type("1.3 Differential").blur();
     cy.get("[id='material']").focus().type("angal.lab.blood").blur();
-    cy.get("[id='panel1bh-header']").click();
-    cy.get("[class='PrivateSwitchBase-input-8']").first().click();
+    cy.get("[id='result']").focus().type("POSITIVE").blur();
     cy.get("[id='note']").focus().type("note").blur();
   });
 
