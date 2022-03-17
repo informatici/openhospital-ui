787e9dd4c107d86e428f9c1cc479c0e5dbeded34
almeida1492 <henriquealmeida1492@gmail.com>
2022-03-17 15:21:24 +0100

    test(OP-815): Add error and success cases to exams.spec

--- cypress/integration/patient_details_activity/exams.spec.js
+++ cypress/integration/patient_details_activity/exams.spec.js
@@ -1,8 +1,7 @@
-const API_BASE_PATH = Cypress.env("API_BASE_PATH");
 const HOSTNAME = Cypress.env("HOSTNAME");
 const START_PATH = `${HOSTNAME}/details/1`;
 
-describe.skip("Patient Details / Exams", () => {
+describe("Patient Details / Exams", () => {
   before(() => {
     cy.authenticate(START_PATH);
   });
@@ -24,25 +23,23 @@ describe.skip("Patient Details / Exams", () => {
   });
 
   it("should display an error dialog if the exam creation call fails", () => {
-    cy.intercept("POST", `${API_BASE_PATH}/laboratories`, {
-      statusCode: 400,
-    }).as("postLaboratories");
+    cy.get("[id='note']").focus().clear().type("ERROR").blur();
 
     cy.get("[class='submit_button']").click();
-
-    cy.wait("@postLaboratories");
 
     cy.get("div.infoBox").should("have.class", "error");
   });
 
   it("should display a dialog confirming the exam creation when the call is successful", () => {
-    cy.intercept("POST", `${API_BASE_PATH}/laboratories`, {
-      statusCode: 201,
-    }).as("postLaboratories");
+    cy.get("[id='note']")
+      .focus()
+      .clear()
+      .type("This is the note of a valid form")
+      .blur();
 
     cy.get("[class='submit_button']").click();
 
-    cy.wait("@postLaboratories");
+    cy.get("div.infoBox").should("not.exist");
 
     cy.get("div.dialog__title").contains("Patient exam added");
     cy.get("div.return_button").click();
