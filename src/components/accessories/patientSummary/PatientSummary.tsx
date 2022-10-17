97066bf36ded2cf7ee46383d8daa9e112e0126c5
SteveGT96 <stevegires@gmail.com>
2022-10-17 14:37:43 +0100

    update(OH2-143): Add allergies and anamnesis inputs to patient clinic section

--- src/components/accessories/patientSummary/PatientSummary.tsx
+++ src/components/accessories/patientSummary/PatientSummary.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getMedicals } from "../../../state/medicals/actions";
+import { PatientExtraData } from "../patientExtraData/patientExtraData";
 import Tabs from "../tabs/Tabs";
 import "./styles.scss";
 import { patientSummaryTabs } from "./tabsConfig";
@@ -13,6 +14,7 @@ const PatientSummary: FunctionComponent = () => {
 
   return (
     <div className="patientSummary">
+      <PatientExtraData />
       <Tabs config={patientSummaryTabs} />
     </div>
   );
