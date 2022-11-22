8b1e99d8e46dde54c55cb2bd51b2b91a72ea4bba
SteveGT96 <stevegires@gmail.com>
2022-11-22 16:41:05 +0100

    update: Update logged user info on dashboard

--- src/components/accessories/dashboard/dashboardContent/SideContent.tsx
+++ src/components/accessories/dashboard/dashboardContent/SideContent.tsx
@@ -1,11 +1,43 @@
 import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { IState } from "../../../../types";
 
 export const SideContent: FC = () => {
+  const { t } = useTranslation();
+  const username = useSelector<IState, string>(
+    (state) => state.main.authentication.data?.username ?? ""
+  );
   return (
     <div className="side">
-      <div className="header"></div>
+      <div className="side__header">
+        <div className="side__header_avatar"></div>
+        <div>
+          <span>{username}</span>
+        </div>
+      </div>
+      <div className="side__body">
+        <div className="section">
+          <span className="title">{t("user.username")}</span>
+          <div className="content">
+            <span>admin</span>
+          </div>
+        </div>
+        <div className="section">
+          <span className="title">{t("user.groups")}</span>
+          <div className="content">
+            <span>Admin</span>
+            <span>Nurser</span>
+            <span>Doctor</span>
+          </div>
+        </div>
+        <div className="section">
+          <span className="title">{t("user.lastlogin")}</span>
+          <div className="content">
+            <span>2022-11-10 - 02:10AM</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
