d4ac9d9982f4b58095944ff485c1fda4eea4ce78
mwithi <alessandro.domanico@yahoo.it>
2023-02-05 22:15:36 +0100

    # WARNING: head commit changed in the meantime
    Workaround: hide everything but dashboard

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Navigate, Route, Routes } from "react-router";
 import Dashboard from "../components/accessories/dashboard/Dashboard";
-import LaboratoryActivity from "../components/activities/laboratoryActivity/LaboratoryActivity";
 import LoginActivity from "../components/activities/loginActivity/LoginActivity";
 import { RedirectAfterLogin } from "../components/activities/loginActivity/RedirectAfterLogin";
 import NotFound from "../components/activities/notFound/NotFound";
-import VisitsActivity from "../components/activities/visitsActivity/VisitsActivity";
 import { Private } from "../components/Private";
-import { PatientsRoutes } from "./Patients/PatientsRoutes";
 import { PATHS } from "../consts";
 
 export const MainRouter: React.FC = () => {
@@ -16,12 +13,12 @@ export const MainRouter: React.FC = () => {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         {/* TODO: based on user profile, redirect to patient, dashboard or whatever */}
-        <Route index element={<Navigate to="/patients" replace />} />
+        <Route index element={<Navigate to="/dashboard" replace />} />
 
         <Route
           path="login"
           element={
-            <RedirectAfterLogin successRoute="/patients">
+            <RedirectAfterLogin successRoute="/dashboard">
               <LoginActivity />
             </RedirectAfterLogin>
           }
@@ -29,12 +26,6 @@ export const MainRouter: React.FC = () => {
 
         <Route element={<Private />}>
           <Route path={`${PATHS.dashboard}`} element={<Dashboard />} />
-          <Route path={`${PATHS.visits}`} element={<VisitsActivity />} />
-          <Route
-            path={`${PATHS.laboratory}/*`}
-            element={<LaboratoryActivity />}
-          />
-          <Route path={`${PATHS.patients}/*`} element={<PatientsRoutes />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
