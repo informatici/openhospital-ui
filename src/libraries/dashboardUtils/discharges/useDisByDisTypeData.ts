bf2c6f779a9d77bcc8abe5edf771ba0d35ab8ab4
SteveGT96 <stevegires@gmail.com>
2023-04-18 15:59:33 +0100

    refactor: Refactor dashboard data computations

---
+++ src/libraries/dashboardUtils/discharges/useDisByDisTypeData.ts
@@ -0,0 +1,62 @@
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { AdmissionDTO, AdmissionTypeDTO } from "../../../generated";
+import { TAPIResponseStatus } from "../../../state/types";
+import { IState } from "../../../types";
+import { generateColor } from "../../uiUtils/colorGenerator";
+
+export const useDisByDisTypeData = () => {
+  const { t } = useTranslation();
+  const admissions = useSelector<IState, AdmissionDTO[]>(
+    (state) => state.admissions.getDischarges.data ?? []
+  );
+  const dischargeTypes = useSelector<IState, AdmissionTypeDTO[]>(
+    (state) => state.dischargeTypes.allDischargeTypes.data ?? []
+  );
+  const dischargeTypeStatus = useSelector<IState, TAPIResponseStatus>(
+    (state) => state.dischargeTypes.allDischargeTypes.status ?? "IDLE"
+  );
+  const status = useSelector<IState, TAPIResponseStatus>(
+    (state) => state.admissions.getDischarges.status ?? "IDLE"
+  );
+  const success = useSelector<IState, boolean>((state) =>
+    ["SUCCESS", "SUCCESS_EMPTY"].includes(
+      state.admissions.getDischarges.status ?? ""
+    )
+  );
+  const labels = dischargeTypes.map((e) => e.description ?? "");
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        data: dischargeTypes.map(
+          (e) => admissions.filter((adm) => adm.disType?.code === e.code).length
+        ),
+        backgroundColor: dischargeTypes.map((e, index) =>
+          generateColor({ red: 255 })
+        ),
+      },
+    ],
+  };
+
+  const csvData = [
+    ...dischargeTypes.map((e) => ({
+      [t("admission.distype")]: e.description ?? "",
+      [t("common.male")]: admissions.filter(
+        (adm) => adm.disType?.code === e.code && adm.patient?.sex === "M"
+      ).length,
+      [t("common.female")]: admissions.filter(
+        (adm) => adm.disType?.code === e.code && adm.patient?.sex === "F"
+      ).length,
+    })),
+  ];
+
+  return {
+    status,
+    dischargeTypeStatus,
+    data,
+    csvData,
+    success,
+    total: admissions.length,
+  };
+};
---
+++ src/libraries/dashboardUtils/opds/useOpdByAgeTypeData.ts
@@ -0,0 +1,70 @@
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { AgeTypeDTO, OpdDTO } from "../../../generated";
+import { TAPIResponseStatus } from "../../../state/types";
+import { IState } from "../../../types";
+
+export const useOpdByAgeTypeData = () => {
+  const { t } = useTranslation();
+  const opds = useSelector<IState, OpdDTO[]>(
+    (state) => state.opds.searchOpds.data ?? []
+  );
+  const ageTypes = useSelector<IState, AgeTypeDTO[]>(
+    (state) => state.ageTypes.getAllAgeTypes.data ?? []
+  );
+  const ageTypeStatus = useSelector<IState, TAPIResponseStatus>(
+    (state) => state.ageTypes.getAllAgeTypes.status ?? "IDLE"
+  );
+  const status = useSelector<IState, TAPIResponseStatus>(
+    (state) => state.opds.searchOpds.status ?? "IDLE"
+  );
+  const success = useSelector<IState, boolean>((state) =>
+    ["SUCCESS", "SUCCESS_EMPTY"].includes(state.opds.searchOpds.status ?? "")
+  );
+  const labels = ageTypes.map((e) => t(`patient.agetypes.${e.code ?? ""}`));
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: t("opd.male"),
+        data: ageTypes.map(
+          (e) =>
+            opds.filter((opd) => opd.ageType === e.code && opd.sex === "M")
+              .length
+        ),
+        backgroundColor: "rgba(255, 99, 132, 0.8)",
+      },
+      {
+        label: t("opd.female"),
+        data: ageTypes.map(
+          (e) =>
+            opds.filter((opd) => opd.ageType === e.code && opd.sex === "F")
+              .length
+        ),
+        backgroundColor: "rgba(54, 162, 235, 0.8)",
+      },
+    ],
+  };
+
+  const csvData = [
+    ...ageTypes.map((e) => ({
+      [t("common.agetype")]: t(`patient.agetypes.${e.code ?? ""}`),
+      [t("common.male")]: opds.filter(
+        (opd) => opd.ageType === e.code && opd.sex === "M"
+      ).length,
+      [t("common.female")]: opds.filter(
+        (opd) => opd.ageType === e.code && opd.sex === "F"
+      ).length,
+    })),
+  ];
+
+  return {
+    ageTypeStatus,
+    status,
+    data,
+    csvData,
+    success,
+    total: opds.length,
+    opds,
+  };
+};
