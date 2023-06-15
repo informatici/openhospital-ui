0b7123722bfd4ecf772304f78c274b067bd93937
Alessandro Domanico <alessandro.domanico@yahoo.it>
2023-06-15 17:38:48 +0200

    Set UI to include credentials in header (#479)
    - set withCredentials = true

--- src/generated/runtime.ts
+++ src/generated/runtime.ts
@@ -104,6 +104,7 @@ export class BaseAPI {
             headers,
             body: body instanceof FormData ? body : JSON.stringify(body),
             responseType: responseType ?? 'json',
+            withCredentials: true,
         };
     }
 
@@ -159,6 +160,7 @@ export interface RequestOpts extends AjaxRequest {
     headers?: HttpHeaders;
     body?: HttpBody;
     responseType?: 'json' | 'blob' | 'arraybuffer' | 'text';
+    withCredentials?: boolean;
 }
 
 export interface ResponseOpts {
