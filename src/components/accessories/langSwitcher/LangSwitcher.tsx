585e83572314e3e918f1cfb8ba270d8116ddfc42
SilverD3 <mrboornesilver@gmail.com>
2023-10-04 17:21:03 +0100

    Display every language in its original spelling

--- src/components/accessories/langSwitcher/LangSwitcher.tsx
+++ src/components/accessories/langSwitcher/LangSwitcher.tsx
@@ -6,34 +6,33 @@ import { useTranslation } from "react-i18next";
 import availableLanguages from "../../../customization/available-languages.json";
 import { isEmpty } from "lodash";
 
-const fallbackLanguages = [
-  "en",
-  "it",
-  "de",
-  "fr",
-  "es",
-  "pt",
-  "ar",
-  "sw",
-  "am",
-  "cs",
-  "sq",
-  "zh",
-];
+const fallbackLanguages: Record<string, string> = {
+  en: "English",
+  it: "Italiano",
+  de: "Deutsch",
+  fr: "Français",
+  es: "Español",
+  pt: "Português",
+  ar: "عرب",
+  sw: "Svenska",
+  am: "አማርኛ",
+  cs: "čeština",
+  sq: "Shqiptar",
+  zh: "中国人",
+};
 
 const LangSwitcher: FunctionComponent = () => {
   const currentLang = i18n.language;
   const { changeLang } = useContext(LangContext);
-  const { t } = useTranslation();
 
   const languages = isEmpty(availableLanguages.availableLanguages)
-    ? fallbackLanguages
+    ? Object.keys(fallbackLanguages)
     : availableLanguages.availableLanguages;
 
   const renderOptions = (): JSX.Element[] => {
     return languages.map((code: string) => (
       <option key={code} value={code}>
-        {t(`languages.${code}`)}
+        {fallbackLanguages[code]}
       </option>
     ));
   };
