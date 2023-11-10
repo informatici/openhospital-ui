6f076ca458381879b70a5c95b702bbd1b25dcdff
Steve Tsala <45661418+SteveGT96@users.noreply.github.com>
2023-11-10 18:48:49 +0100

    OH2-251 | Fix visit filter not working correctly (#532)
    * fix(OH2-251): Fix visit newPatient not being set correctly
    
    * update(OH2-251): Rename 'new admittance' to 'new attendance'
    
    * style: Remove unused imports

--- src/components/accessories/opds/filter/OpdFilterForm.tsx
+++ src/components/accessories/opds/filter/OpdFilterForm.tsx
@@ -9,10 +9,10 @@ import { differenceInSeconds } from "date-fns";
 import { useFormik } from "formik";
 import get from "lodash.get";
 import has from "lodash.has";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { FC, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { date, number, object, string } from "yup";
+import { number, object, string } from "yup";
 import {
   DiseaseDTO,
   DiseaseTypeDTO,
@@ -311,11 +311,11 @@ export const OpdFilterForm: FC<IOpdFilterProps> = ({
   const newPatientOptions = [
     {
       label: t("opd.all"),
-      value: "",
+      value: "A",
     },
     {
-      label: t("opd.newadmittance"),
-      value: "A",
+      label: t("opd.newattendance"),
+      value: "N",
     },
     {
       label: t("opd.reattendance"),
