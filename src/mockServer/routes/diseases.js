5e52f2a2a41cd66899d476c6b8037d28e91026b4
Gaspard Beernaert <gaspard@gmail.com>
2024-05-27 22:41:38 +0900

    fix(fixtures): make fixtures more expresive / easier to test with (#589)
    * fix(fixtures): make fixtures more expresive / easier to test with
    
    * feat(fixtures): adding another disease

--- src/mockServer/routes/diseases.js
+++ src/mockServer/routes/diseases.js
@@ -1,18 +1,22 @@
-import { diseaseDTO } from "../fixtures/diseaseDTO";
+import { diseasesDTO } from "../fixtures/diseasesDTO";
 
 export const diseasesRoutes = (server) => {
   server.namespace("/diseases", () => {
-    server.get("/all").intercept((req, res) => {
-      res.status(200).json([diseaseDTO, diseaseDTO, diseaseDTO, diseaseDTO]);
+    server.get("/all").intercept((_req, res) => {
+      res.status(200).json(diseasesDTO);
     });
-    server.get("/opd").intercept((req, res) => {
-      res.status(200).json([diseaseDTO, diseaseDTO, diseaseDTO, diseaseDTO]);
+    server.get("/opd").intercept((_req, res) => {
+      res.status(200).json(diseasesDTO.filter(({ opdInclude }) => opdInclude));
     });
-    server.get("/ipd/in").intercept((req, res) => {
-      res.status(200).json([diseaseDTO, diseaseDTO, diseaseDTO, diseaseDTO]);
+    server.get("/ipd/in").intercept((_req, res) => {
+      res
+        .status(200)
+        .json(diseasesDTO.filter(({ ipdInInclude }) => ipdInInclude));
     });
-    server.get("/ipd/out").intercept((req, res) => {
-      res.status(200).json([diseaseDTO, diseaseDTO, diseaseDTO, diseaseDTO]);
+    server.get("/ipd/out").intercept((_req, res) => {
+      res
+        .status(200)
+        .json(diseasesDTO.filter(({ ipdOutInclude }) => ipdOutInclude));
     });
   });
 };
