bca76d3774569eed753b12992a71d668666a72f3
SteveGT96 <steve.tsala@intesys.it>
2024-07-31 17:44:59 +0100

    fix: Fix search patient item click handler

--- src/components/activities/searchPatientActivity/PatientSearchItem.tsx
+++ src/components/activities/searchPatientActivity/PatientSearchItem.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Navigate } from "react-router";
+import { Navigate, useNavigate } from "react-router";
 import { PatientDTOStatusEnum } from "../../../generated";
 import { renderDate } from "../../../libraries/formatUtils/dataFormatting";
 import { ProfilePicture } from "../../accessories/profilePicture/ProfilePicture";
@@ -12,100 +12,92 @@ const PatientSearchItem: FunctionComponent<IPatientSearchItemProps> = ({
   hideAdditionalInformation = false,
 }) => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const [activityTransitionState, setActivityTransitionState] =
     useState<TActivityTransitionState>("IDLE");
 
   useEffect(() => {
-    if (
-      getPatientSuccessCallback &&
-      activityTransitionState === "TO_PATIENT_DETAILS"
-    ) {
-      getPatientSuccessCallback(patient);
+    if (activityTransitionState === "TO_PATIENT_DETAILS") {
+      if (getPatientSuccessCallback) {
+        getPatientSuccessCallback(patient);
+      } else {
+        navigate(`/patients/details/${patient.code}`);
+      }
     }
   }, [activityTransitionState, getPatientSuccessCallback, patient]);
 
   const patientDate = renderDate(patient.birthDate ?? "");
 
-  switch (activityTransitionState) {
-    case "TO_PATIENT_DETAILS":
-      return getPatientSuccessCallback ? (
-        <Navigate to={`/patients/details/${patient.code}`} />
-      ) : null;
-    default:
-      return (
-        <div
-          data-cy="patient-search-item"
-          className="patientSearchItem col-xs-12"
-        >
-          <div
-            className="patientSearchItem__panel"
-            onClick={() => setActivityTransitionState("TO_PATIENT_DETAILS")}
-          >
-            <div className="patientSearchItem__profile">
-              <div className="patientSearchItem__profile__picture">
-                <ProfilePicture
-                  isEditable={false}
-                  preLoadedPicture={patient.blobPhoto}
-                />
+  return (
+    <div data-cy="patient-search-item" className="patientSearchItem col-xs-12">
+      <div
+        className="patientSearchItem__panel"
+        onClick={() => setActivityTransitionState("TO_PATIENT_DETAILS")}
+      >
+        <div className="patientSearchItem__profile">
+          <div className="patientSearchItem__profile__picture">
+            <ProfilePicture
+              isEditable={false}
+              preLoadedPicture={patient.blobPhoto}
+            />
+          </div>
+          <div className="patientSearchItem__profile__content">
+            <div className="patientSearchItem__profile__content__name">
+              {`${patient.firstName || ""} ${patient.secondName || ""}`}
+            </div>
+            <div className="patientSearchItem__profile__content__status">
+              {patient?.status === PatientDTOStatusEnum.I ? (
+                <div className="patientDetails_status_wrapper patientDetails_status_in">
+                  <h6>
+                    {t("patient.status")}: <span>{t("patient.instatus")}</span>
+                  </h6>
+                </div>
+              ) : (
+                <div className="patientDetails_status_wrapper patientDetails_status_out">
+                  <h6>
+                    {t("patient.status")}: <span>{t("patient.outstatus")}</span>
+                  </h6>
+                </div>
+              )}
+            </div>
+            <div className="patientSearchItem__profile__content__info">
+              <div className="patientSearchItem__profile__content__item">
+                <strong>{t("patient.patientID")}:</strong> {patient.code}
               </div>
-              <div className="patientSearchItem__profile__content">
-                <div className="patientSearchItem__profile__content__name">
-                  {`${patient.firstName || ""} ${patient.secondName || ""}`}
+              <div className="patientSearchItem__profile__content__item">
+                <strong>{t("patient.sex")}:</strong> {patient.sex || "-"}
+              </div>
+              {!hideAdditionalInformation && (
+                <div className="patientSearchItem__profile__content__item">
+                  <strong>{t("patient.birthdate")}:</strong> {patientDate}
                 </div>
-                <div className="patientSearchItem__profile__content__status">
-                  {patient?.status === PatientDTOStatusEnum.I ? (
-                    <div className="patientDetails_status_wrapper patientDetails_status_in">
-                      <h6>
-                      {t("patient.status")}: <span>{t("patient.instatus")}</span>
-                      </h6>
-                    </div>
-                  ) : (
-                    <div className="patientDetails_status_wrapper patientDetails_status_out">
-                      <h6>
-                      {t("patient.status")}: <span>{t("patient.outstatus")}</span>
-                      </h6>
-                    </div>
-                  )}
+              )}
+              {!hideAdditionalInformation && (
+                <div className="patientSearchItem__profile__content__item">
+                  <strong>{t("patient.hasinsurance")}:</strong>{" "}
+                  {patient.hasInsurance || "-"}
                 </div>
-                <div className="patientSearchItem__profile__content__info">
-                  <div className="patientSearchItem__profile__content__item">
-                    <strong>{t("patient.patientID")}:</strong> {patient.code}
-                  </div>
-                  <div className="patientSearchItem__profile__content__item">
-                    <strong>{t("patient.sex")}:</strong> {patient.sex || "-"}
-                  </div>
-                  {!hideAdditionalInformation && (
-                    <div className="patientSearchItem__profile__content__item">
-                      <strong>{t("patient.birthdate")}:</strong> {patientDate}
-                    </div>
-                  )}
-                  {!hideAdditionalInformation && (
-                    <div className="patientSearchItem__profile__content__item">
-                      <strong>{t("patient.hasinsurance")}:</strong>{" "}
-                      {patient.hasInsurance || "-"}
-                    </div>
-                  )}
+              )}
+            </div>
+            <div className="patientSearchItem__profile__content__contact">
+              {!hideAdditionalInformation && (
+                <div className="patientSearchItem__profile__content__item">
+                  <strong>{t("patient.address")}:</strong>{" "}
+                  {`${patient.address || ""} ${
+                    patient.city ? " - " + patient.city : ""
+                  }`}
                 </div>
-                <div className="patientSearchItem__profile__content__contact">
-                  {!hideAdditionalInformation && (
-                    <div className="patientSearchItem__profile__content__item">
-                      <strong>{t("patient.address")}:</strong>{" "}
-                      {`${patient.address || ""} ${
-                        patient.city ? " - " + patient.city : ""
-                      }`}
-                    </div>
-                  )}
-                  <div className="patientSearchItem__profile__content__item">
-                    <strong>{t("patient.telephone")}:</strong>{" "}
-                    {patient.telephone || "-"}
-                  </div>
-                </div>
+              )}
+              <div className="patientSearchItem__profile__content__item">
+                <strong>{t("patient.telephone")}:</strong>{" "}
+                {patient.telephone || "-"}
               </div>
             </div>
           </div>
         </div>
-      );
-  }
+      </div>
+    </div>
+  );
 };
 
 export default PatientSearchItem;
