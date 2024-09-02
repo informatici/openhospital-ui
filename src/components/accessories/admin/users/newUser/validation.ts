60f88c1b10ef9325e10fab7a269849b7cee1fe44
Gaspard Beernaert <gaspard@gmail.com>
2024-09-02 12:17:28 +0200

    fix: restrict userName characters (#646)

--- src/components/accessories/admin/users/newUser/validation.ts
+++ src/components/accessories/admin/users/newUser/validation.ts
@@ -1,13 +1,19 @@
-import { object, string, ref } from "yup";
-import { UserGroupDTO } from "../../../../../generated";
 import { TFunction } from "react-i18next";
+import { object, ref, string } from "yup";
+import { UserGroupDTO } from "../../../../../generated";
 import { FormProps } from "./NewUser";
 // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 export const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
+export const userNameRules = /^[a-z0-9-._]+$/;
+
 export const userSchema = (t: TFunction<"translation">) =>
   object().shape<FormProps>({
-    userName: string().min(2).required(t("user.validateUserName")),
+    userName: string()
+      .min(2)
+      .max(50)
+      .matches(userNameRules, t("user.validateUserNameRegex"))
+      .required(t("user.validateUserName")),
     userGroupName: object<UserGroupDTO>({
       code: string().required(t("user.validateUserNeedsGroup")),
       desc: string(),
