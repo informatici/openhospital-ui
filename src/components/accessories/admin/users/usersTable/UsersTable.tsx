a204193a85cdf5c7ee81a031744f91c32b1cef0b
SteveGT96 <steve.tsala@intesys.it>
2024-10-23 15:19:33 +0100

    feature(OH2-300): Implement delete user feature

--- src/components/accessories/admin/users/usersTable/UsersTable.tsx
+++ src/components/accessories/admin/users/usersTable/UsersTable.tsx
@@ -1,32 +1,56 @@
 import { CircularProgress } from "@mui/material";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 import { useAppDispatch, useAppSelector } from "libraries/hooks/redux";
 import { usePermission } from "libraries/permissionUtils/usePermission";
 import { UserDTO } from "../../../../../generated";
-import { getUsers } from "../../../../../state/users";
+import { deleteUserReset, getUsers } from "../../../../../state/users";
 import InfoBox from "../../../infoBox/InfoBox";
 import Table from "../../../table/Table";
 import { TFilterField } from "../../../table/filter/types";
 
+import { scrollToElement } from "libraries/uiUtils/scrollToElement";
 import { getUserGroups } from "state/usergroups";
 import classes from "./UsersTable.module.scss";
 
 interface IOwnProps {
   headerActions: ReactNode;
   onEdit: (row: UserDTO) => void;
+  onDelete: (row: UserDTO) => void;
 }
 
-export const UsersTable = ({ headerActions, onEdit }: IOwnProps) => {
+export const UsersTable = ({ headerActions, onEdit, onDelete }: IOwnProps) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
+  const infoBoxRef = useRef<HTMLDivElement>(null);
+
   const canUpdate = usePermission("users.update");
+  const canDelete = usePermission("users.update");
+
+  const deleteUser = useAppSelector((state) => state.users.delete);
 
   useEffect(() => {
     dispatch(getUsers({}));
     dispatch(getUserGroups());
+
+    return () => {
+      dispatch(deleteUserReset());
+    };
   }, [dispatch]);
+
+  useEffect(() => {
+    if (deleteUser.status === "FAIL") {
+      scrollToElement(infoBoxRef.current);
+    }
+
+    if (
+      deleteUser.status === "SUCCESS" ||
+      deleteUser.status === "SUCCESS_EMPTY"
+    ) {
+      dispatch(getUsers({}));
+    }
+  }, [deleteUser.status, dispatch]);
 
   const header = ["userName", "userGroupName", "desc"];
 
@@ -65,7 +89,7 @@ export const UsersTable = ({ headerActions, onEdit }: IOwnProps) => {
         userGroupName:
           item.userGroupName?.desc ?? item.userGroupName?.code ?? "",
         desc: item.desc ?? "",
-        passwd: item.passwd ?? ""
+        passwd: item.passwd ?? "",
       };
     });
   };
@@ -85,23 +109,31 @@ export const UsersTable = ({ headerActions, onEdit }: IOwnProps) => {
 
           case "SUCCESS":
             return (
-              <Table
-                rowData={formatDataToDisplay(data ?? [])}
-                tableHeader={header}
-                labelData={label}
-                columnsOrder={order}
-                rowsPerPage={10}
-                filterColumns={filters}
-                manualFilter={false}
-                isCollapsabile={false}
-                rawData={(data ?? []).map((user) => ({
-                  ...user,
-                  userGroupName: user.userGroupName?.code,
-                }))}
-                rowKey="userName"
-                headerActions={headerActions}
-                onEdit={canUpdate ? onEdit: undefined}
-              />
+              <>
+                {deleteUser.status === "FAIL" && (
+                  <div ref={infoBoxRef} className="info-box-container">
+                    <InfoBox type="error" message={deleteUser.error?.message} />
+                  </div>
+                )}
+                <Table
+                  rowData={formatDataToDisplay(data ?? [])}
+                  tableHeader={header}
+                  labelData={label}
+                  columnsOrder={order}
+                  rowsPerPage={10}
+                  filterColumns={filters}
+                  manualFilter={false}
+                  isCollapsabile={false}
+                  rawData={(data ?? []).map((user) => ({
+                    ...user,
+                    userGroupName: user.userGroupName?.code,
+                  }))}
+                  rowKey="userName"
+                  headerActions={headerActions}
+                  onEdit={canUpdate ? onEdit : undefined}
+                  onDelete={canDelete ? onDelete : undefined}
+                />
+              </>
             );
           case "SUCCESS_EMPTY":
             return <InfoBox type="info" message={t("common.emptydata")} />;
