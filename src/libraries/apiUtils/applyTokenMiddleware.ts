37029d8992e84c365563e628f56e4219b6b31fda
SteveGT96 <steve.tsala@intesys.it>
2024-11-19 11:19:15 +0100

    update: Update token middleware

--- src/libraries/apiUtils/applyTokenMiddleware.ts
+++ src/libraries/apiUtils/applyTokenMiddleware.ts
@@ -2,16 +2,20 @@ import produce from "immer";
 import { set } from "lodash";
 import { AUTH_KEY } from "../../consts";
 import { Middleware, RequestArgs } from "../../generated";
+import { refreshTokenHasExpired } from "../authUtils/tokenHasExpired";
 import { SessionStorage } from "../storage/storage";
-import { redirect } from "react-router";
-import { tokenHasExpired } from "../authUtils/tokenHasExpired";
 
 export const applyTokenMiddleware: Middleware = {
   pre(request: RequestArgs): RequestArgs {
     const userCredentials = SessionStorage.read(AUTH_KEY);
-    if (userCredentials.token && tokenHasExpired(userCredentials.token)) {
+    if (
+      userCredentials?.refreshToken &&
+      refreshTokenHasExpired(userCredentials.refreshToken)
+    ) {
       SessionStorage.clear();
-      return redirect("/login");
+    }
+    if (!userCredentials) {
+      window.location.reload();
     }
     return produce(request, (draft) => {
       if (userCredentials.token) {
