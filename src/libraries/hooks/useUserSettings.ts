1b7991d9fb0cf5f692e9bb42fcda460194ea7522
Alessandro Falezza <alessandro.falezza@intesys.it>
2025-04-11 15:34:01 +0200

    Fix redux typescript (#715)
    * Fix eslint issues on login page
    
    * Mock settings api
    
    * Fix dashboard selector
    
    ---------
    
    Co-authored-by: Alessandro Falezza <alessandro.falezza@intesyt.it>

--- src/libraries/hooks/useUserSettings.ts
+++ src/libraries/hooks/useUserSettings.ts
@@ -1,7 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { useAppSelector } from "libraries/hooks/redux";
+import { UserSettingDTO } from "../../generated";
+
+const userSettingsSelector = createSelector(
+  (state) => state.main.settings?.data,
+  (settings) => settings || []
+);
 
 /**
  * @returns list of all user settings
  */
 export const useUserSettings = () =>
-  useAppSelector((state) => state.main.settings?.data || []);
+  useAppSelector<any, UserSettingDTO[]>(userSettingsSelector);
