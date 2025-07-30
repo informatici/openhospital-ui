2e2f2fc144367b71bb7e12681309b3dddb4d73b9
ArnaudFofou <gui.fofou@umi2growcameroun.com>
2025-07-30 17:43:58 +0100

    fix conflict and bugs

--- src/components/accessories/patientSummary/PatientSummary.tsx
+++ src/components/accessories/patientSummary/PatientSummary.tsx
@@ -24,10 +24,10 @@ const PatientSummary: FunctionComponent = () => {
     { label: t("common.orderbytype"), content: <PatientSummaryByType /> },
   ];
   const handlePrint = () => {
-    printSubject.next();
+    printSubject.next(null);
     setTimeout(() => {
       window.print();
-    }, 1000);
+    }, 700);
   };
 
   return (
@@ -38,7 +38,7 @@ const PatientSummary: FunctionComponent = () => {
         onClick={handlePrint}
         variant="contained"
       >
-        {t("common.print_data")}
+        {t("common.printData")}
       </Button>
       <PatientExtraData readOnly={true} />
       <Tabs config={patientSummaryTabs} />
