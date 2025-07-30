01d59603ea13476feba9b973915c0c92e94b1a49
ArnaudFofou <gui.fofou@umi2growcameroun.com>
2025-07-30 10:58:10 +0100

    feat(OH2-466): add middleware to set user Language

---
+++ src/libraries/apiUtils/applyLanguageMiddleware.ts
@@ -0,0 +1,15 @@
+import { LAGUAGE_KEY } from "consts";
+import produce from "immer";
+import { set } from "lodash";
+import { Middleware, RequestArgs } from "../../generated";
+import { LocalStorage } from "../storage/storage";
+
+export const applyLanguageMiddleware: Middleware = {
+  pre(request: RequestArgs): RequestArgs {
+    const lang = LocalStorage.read(LAGUAGE_KEY);
+    console.log(lang);
+    return produce(request, (draft) => {
+      draft.headers = set(draft.headers || {}, "accept-language", lang);
+    });
+  },
+};
