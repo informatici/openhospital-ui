852ce980ffadc158cefb24ae752dc9bb9c64274a
SteveGT96 <steve.tsala@intesys.it>
2025-08-18 11:52:45 +0100

    chore(OH2-27): Add encounter translations

--- src/components/accessories/encounters/Encounters.tsx
+++ src/components/accessories/encounters/Encounters.tsx
@@ -1,10 +1,12 @@
 import "moment/min/locales";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const Encounters = () => {
+  const { t } = useTranslation();
   return (
     <div className="encounters">
-      <h1>Encounters</h1>
+      <h1>{t("patient.encounters")}</h1>
     </div>
   );
 };
