251cd8081ca8b000094f677fa1c3646bbdf91f54
Aurorj <aurellejoellen@gmail.com>
2025-11-19 06:51:41 +0100

    fix/OH2-192 Add end-to-end tests for new pharmaceutical creation

--- cypress/integrations/pharmacy/pharmaceutical/new-pharmaceutical.cy.ts
+++ cypress/integrations/pharmacy/pharmaceutical/new-pharmaceutical.cy.ts
@@ -1,12 +1,29 @@
 describe("New Pharmaceutical", () => {
-  beforeEach(() => {
+  it("Should render the UI", () => {
     cy.authenticate("/pharmacy/pharmaceutical/new");
-    cy.dataCy("pharmaceutical-form").should("exist");
+    cy.dataCy("new-pharmaceutical").should("exist");
   });
 
-  it("should display form fields and actions", () => {
+  it("Should display form fields", () => {
+    cy.dataCy("pharmaceutical-form").should("exist");
     cy.byId("prodCode").should("exist");
     cy.byId("type").should("exist");
     cy.dataCy("submit-button").should("exist");
+  });
+
+  it("Should fill form", () => {
+    cy.byId("prodCode").type("TEST001");
+    cy.byId("type")
+      .should("not.be.disabled")
+      .click()
+      .type("{downarrow}{enter}");
+    cy.byId("description").type("Test description");
+    cy.byId("pcsperpck").type("10");
+    cy.byId("minqty").type("5");
+  });
+
+  it("Should submit form successfully", () => {
+    cy.dataCy("submit-button").click();
+    cy.url().should("include", "/pharmacy/pharmaceutical");
   });
 });
