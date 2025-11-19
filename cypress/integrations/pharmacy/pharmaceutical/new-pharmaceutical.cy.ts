eb8c68dfe7f5c75b4979494c9b6990fd10547121
Aurorj <aurellejoellen@gmail.com>
2025-11-19 14:06:04 +0100

    Update/Add new pharmaceutical e2e tests

--- cypress/integrations/pharmacy/pharmaceutical/new-pharmaceutical.cy.ts
+++ cypress/integrations/pharmacy/pharmaceutical/new-pharmaceutical.cy.ts
@@ -1,29 +1,39 @@
 describe("New Pharmaceutical", () => {
-  it("Should render the UI", () => {
+  beforeEach(() => {
     cy.authenticate("/pharmacy/pharmaceutical/new");
+  });
+
+  it("Should render the UI", () => {
     cy.dataCy("new-pharmaceutical").should("exist");
   });
 
-  it("Should display form fields", () => {
+  it("Should navigate to pharmaceutical form", () => {
     cy.dataCy("pharmaceutical-form").should("exist");
-    cy.byId("prodCode").should("exist");
-    cy.byId("type").should("exist");
-    cy.dataCy("submit-button").should("exist");
   });
 
-  it("Should fill form", () => {
-    cy.byId("prodCode").type("TEST001");
-    cy.byId("type")
-      .should("not.be.disabled")
-      .click()
-      .type("{downarrow}{enter}");
-    cy.byId("description").type("Test description");
-    cy.byId("pcsperpck").type("10");
-    cy.byId("minqty").type("5");
+  it("Should fill pharmaceutical form", () => {
+    cy.byId("prodCode").focus().clear().type("PROD_04");
+    cy.byId("type").focus().clear().type("Labora");
+    cy.byId("type-option-0").click();
+    cy.byId("description").focus().clear().type("fail");
+    cy.byId("pcsperpck").focus().clear().type("25");
+    cy.byId("minqty").focus().clear().type("75");
   });
 
-  it("Should submit form successfully", () => {
+  it("Should display an error info box if the pharmaceutical creation fails", () => {
     cy.dataCy("submit-button").click();
-    cy.url().should("include", "/pharmacy/pharmaceutical");
+
+    cy.dataCy("info-box").should("have.class", "error");
+  });
+
+  it("Should show a confirmation if the pharmaceutical creation succeeds", () => {
+    cy.byId("prodCode").focus().clear().type("PROD_04");
+    cy.byId("type").focus().clear().type("Labora");
+    cy.byId("type-option-0").click();
+    cy.byId("description").focus().clear().type("New description").blur();
+    cy.dataCy("submit-button").click();
+    cy.dataCy("info-box").should("not.exist");
+    cy.dataCy("dialog-title").contains("Pharmaceutical added successfully");
+    cy.dataCy("approve-dialog").click();
   });
 });
