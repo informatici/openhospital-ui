62ce23a4734fcbf371ac5bac31073c1fbc830931
Aurorj <aurellejoellen@gmail.com>
2025-11-25 10:29:58 +0100

    OH2-199 /expanded row content

--- src/components/accessories/table/TableBodyRow.tsx
+++ src/components/accessories/table/TableBodyRow.tsx
@@ -2,17 +2,12 @@ import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 import { Collapse, IconButton } from "@mui/material";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import CellContent from "components/activities/pharmacyActivity/pharmaceutical/components/PharmacyCellContent/CellContent";
 import _ from "lodash";
-import React, {
-  FunctionComponent,
-  useEffect,
-  useMemo,
-} from "react";
+import React, { FunctionComponent, useEffect, useMemo } from "react";
+import { useTranslation } from "react-i18next";
 import "./styles.scss";
 import { IRowProps } from "./types";
-import Button from "../button/Button";
-import { useTranslation } from "react-i18next";
-import CellContent from "components/activities/pharmacyActivity/pharmaceutical/components/PharmacyCellContent/CellContent";
 
 const TableBodyRow: FunctionComponent<IRowProps> = ({
   row,
@@ -29,7 +24,7 @@ const TableBodyRow: FunctionComponent<IRowProps> = ({
   expanded,
   dateFields,
   detailsExcludedFields,
-  adjustQuantity = false,
+  renderExtraContent,
 }) => {
   const [open, setOpen] = React.useState(false);
   const { t } = useTranslation();
@@ -136,11 +131,7 @@ const TableBodyRow: FunctionComponent<IRowProps> = ({
                           )
                       )}
                   </ul>
-                  {adjustQuantity && row.type === "Charge" && (
-                    <Button type="button" variant="outlined" color="inherit">
-                      {t("pharmacy.stock.adjustQuantity")}
-                    </Button>
-                  )}
+                  {renderExtraContent && renderExtraContent(row)}
                 </div>
               )}
             </Collapse>
--- src/components/accessories/table/types.ts
+++ src/components/accessories/table/types.ts
@@ -33,6 +33,7 @@ export interface IProps {
   disableRowAction?: (row: any, action: TActions) => boolean;
   detailsExcludedFields?: string[];
   adjustQuantity?: boolean;
+  renderExtraContent?: (row: any) => React.ReactNode;
   /**
    * Column used for the filter. When this prop is provided, don't forget to provide also [rawData]
    */
@@ -82,6 +83,7 @@ export interface IRowProps {
   dateFields: Array<string>;
   detailsExcludedFields?: string[];
   adjustQuantity?: boolean;
+  renderExtraContent?: (row: any) => ReactNode;
 }
 
 export type TActions =
